4a7e419097cb22037827007441e28235977dfa00
GamingBud <jangrobonerf@gmail.com>
2018-01-05 12:02:52 -0600

    Add files via upload

---
+++ DefChat/DefChat.go
@@ -0,0 +1,15 @@
+components {
+  id: "DefChat_GUI"
+  component: "/DefChat/DefChat.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
