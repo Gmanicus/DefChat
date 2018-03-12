717e348d5584f5e069a8ea8e3ce058062e77458d
GamingBud <jangrobonerf@gmail.com>
2018-03-12 16:07:51 -0500

    Add files via upload

---
+++ DefChat_properties.lua
@@ -0,0 +1,12 @@
+Properties = {
+
+	fonts = {
+		default = {name = hash("Example")},							-- Set fonts for DEF_CHAT to use
+		Second_Example = {name = hash("Second_Example")}
+	},
+
+	stamp_text_instant = true,
+
+	commandline_color = vmath.vector4(.8,.8,.8,0.3)
+	
+}
