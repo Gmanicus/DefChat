94b6e69da0abf3f8cd4367d1d1a961a440e373a8
GamingBud <jangrobonerf@gmail.com>
2018-03-12 16:25:34 -0500

    Added modifiable properties

--- DefChat_properties.lua
+++ DefChat_properties.lua
@@ -5,8 +5,20 @@ Properties = {
 		Second_Example = {name = hash("Second_Example")}
 	},
 
-	stamp_text_instant = true,
+	commands = {													-- Commands to be executed after '>' key. Runs function named under 'func'.
 
-	commandline_color = vmath.vector4(.8,.8,.8,0.3)
+		{
+			command = "clear",
+			func = "clear_chat"
+		}
+
+	},
+
+	stamp_text_instant = true,										-- Set stamp property. (True) instant messasge. (False) stamped message.
+
+	caret_speed = 0.48,												-- Set timing properties. Floor value is monitor refresh rate.
+	stamp_speed = 0.001,
+
+	commandline_color = vmath.vector4(.8,.8,.8,0.3)					-- Set command line color.
 	
 }
